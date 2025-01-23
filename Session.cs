e2b7e8dffae52bee68399aabe332aa16a5f8b59c
thalys <thalys@live.com>
2025-01-23 09:25:43 -0300

    forwarding com servidor terceiro

--- Session.cs
+++ Session.cs
@@ -10,7 +10,36 @@ public class Session(TcpClient peer, IPEndPoint proxyEndpoint)
     {
         Log.Information("Peer connected: {RemoteAddr} -> {LocalAddr}", peer.Client.RemoteEndPoint,
             peer.Client.LocalEndPoint);
+        
+        var proxy = new TcpClient();
+        
+        var isConnected = await ProxyConnectedAsync(proxy, proxyEndpoint);
+
+        if (!isConnected)
+        {
+            Log.Error("Could not connect to proxy");
+
+            return;
+        }
+        
+        Log.Information("Proxy connected: {LocalAddr} -> {RemoteAddr}", proxy.Client.LocalEndPoint,
+            proxy.Client.RemoteEndPoint);
 
         await using var peerNetStream = peer.GetStream();
+        await using var proxyNetStream = proxy.GetStream();
+    }
+
+    private static async Task<bool> ProxyConnectedAsync(TcpClient proxy, IPEndPoint proxyEndpoint)
+    {
+        try
+        {
+            await proxy.ConnectAsync(proxyEndpoint.Address, proxyEndpoint.Port);
+
+            return true;
+        }
+        catch (SocketException)
+        {
+            return false;
+        }
     }
 }
