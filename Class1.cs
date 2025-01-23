67ecfc1d8d12cf440c4072e7d0044493b24654ce
thalys <thalys@live.com>
2025-01-23 08:57:05 -0300

    servidor tcp

--- Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace ProxyNET;
-
-public class Class1
-{
-}
---
+++ ProxyNET.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+    .WriteTo.Console(
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json");
+
+var configuration = builder.Build();
+
+
+var addr = IPAddress.Parse(configuration.GetValue<string>("Address")!);
+var port = configuration.GetValue<int>("Port");
+
+var listenEndpoint = new IPEndPoint(addr, port);
+
+var listener = new TcpListener(listenEndpoint);
+
+Console.WriteLine($"Listening on port {port}");
